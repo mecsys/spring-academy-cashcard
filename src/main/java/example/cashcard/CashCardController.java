683864705d0e72b079a1cac357cf05952220ae52
Isaac Mecchi <isaac.mecchi@sensedia.com>
2024-05-01 18:52:41 -0300

    Add get all resources.

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,12 +1,22 @@
 package example.cashcard;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Sort;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -14,16 +24,48 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+  private final CashCardRepository cashCardRepository;
+  
+  private CashCardController(CashCardRepository cashCardRepository) {
+    this.cashCardRepository = cashCardRepository;
+  }
+
   @GetMapping("/{requestedId}")
   private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-
-    if(requestedId.equals(99L)) {
-      CashCard cashCard = new CashCard(99L, 123.45);
-      return ResponseEntity.ok(cashCard);
+    Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+    if (cashCardOptional.isPresent()) {
+      return ResponseEntity.ok(cashCardOptional.get());
     } else {
       return ResponseEntity.notFound().build();
     }
-    
   }
+
+  @PostMapping
+  private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+
+    CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+    URI locationOfNewCashCard = ucb
+            .path("cashcards/{id}")
+            .buildAndExpand(savedCashCard.id())
+            .toUri();
+
+    return ResponseEntity
+            .created(locationOfNewCashCard)
+            .build();
+
+  }
+
+  @GetMapping()
+  public ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+
+      Page<CashCard> page = cashCardRepository.findAll(
+              PageRequest.of(
+                      pageable.getPageNumber(),
+                      pageable.getPateSize(),
+                      pageable.getSortOr(Sort.by(Sort.Direction.DESC, "amount"))));
+
+      return ResponseEntity.ok(page.getContent());
+  }
+  
 
 }
--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -8,8 +8,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
@@ -39,6 +43,24 @@ class CashCardApplicationTests {
 
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 		assertThat(response.getBody()).isBlank();
+	}
+
+	@Test
+	void shouldCreateNewCashCard() {
+		CashCard newCashCard = new CashCard(null, 250.00);
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+		Number id = documentContext.read("$.id");
+		Double amount = documentContext.read("$.amount");
+
+		assertThat(id).isNotNull();
+		assertThat(amount).isEqualTo(250.00);
 	}
 
 }
